8ac75dd8252389611a5e7ce47fba201cc1861bf0
Dou24 <doueduar2018@gmail.com>
2024-09-21 00:56:52 -0600

    add create-employee

--- proyect1/src/app/pages/adminPages/create-empleado/create-empleado.component.ts
+++ proyect1/src/app/pages/adminPages/create-empleado/create-empleado.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,signal} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AsyncPipe} from '@angular/common';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-create-empleado',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './create-empleado.component.html',
-  styleUrl: './create-empleado.component.css'
+  styleUrl: './create-empleado.component.css',
 })
-export class CreateEmpleadoComponent {
+export class CreateEmpleadoComponent  implements OnInit{
 
+  ngOnInit() {
+  }
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+  
 }
